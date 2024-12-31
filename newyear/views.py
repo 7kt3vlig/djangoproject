7c0d75466812f30a9cfbca1607c95e32f253e5e5
Peter <p3t3rcr3ations@gmail.com>
2024-12-31 18:55:50 +0000

    ?

--- newyear/views.py
+++ newyear/views.py
@@ -4,6 +4,6 @@ from datetime import datetime
 
 # Create your views here.
 def index(request):
-    now = datetime.datetime.now() 
+    now = datetime.now() 
     return render(request, "newyear/index.html", 
                   {"newyear": now.month==1 and now.day ==1})
