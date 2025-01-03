34840df8aa6ff55de62b022205837ce7efdba58d
Peter <p3t3rcr3ations@gmail.com>
2025-01-03 14:29:44 +0000

    updates

--- tasks/views.py
+++ tasks/views.py
@@ -7,11 +7,13 @@ class NewTaskForm(forms.Form):
     task = forms.CharField(label="New task")
     priority = forms.IntegerField(label="priority", min_value=1 , max_value=10)
 
-tasks  = []
 
 # Create your views here.
 def index(request): 
-    return render(request, "tasks/index.html", {"tasks": tasks})
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    return render(request, "tasks/index.html", {
+        "tasks": request.session['tasks']})
 
 
 def add(request):
@@ -19,7 +21,7 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task) 
+            request.session["tasjs"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else: 
             return render(request, "tasks/add.html", {
